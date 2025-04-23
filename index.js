8d7c0880bdc9844c2b92292bed95ef19218225bf
Jose Mata <rafaelmm899@hotmail.com>
2025-04-23 18:39:04 -0300

    added file upload and delete functionality

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import upload from './services/UploadFileService.js';
+import { unlink, existsSync } from 'fs';
 
 const DEFAULT_PORT = 3000;
 const port = process.env.PORT || DEFAULT_PORT;
@@ -6,6 +8,26 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.send('File uploaded successfully');
+});
+
+app.delete('/files/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const filePath = `./uploads/${filename}`;
+    if (!existsSync(filePath)) {
+        return res.status(404).send('File not found for deletion');
+    }
+
+    unlink(filePath, (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error deleting file');
+        }
+    });
+    res.send('File deleted successfully');
 });
 
 const serverRunningHandler = () => console.log(`Server running on port ${port}...`);
